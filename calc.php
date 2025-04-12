7277d62a187c9d3082d119a2b216ead51edf3d7e
Pradeep yadav <pradeep.yadav@enigmaticsmile.com>
2025-04-13 01:14:55 +0530

    Added multiplication functionality

---
+++ calc.php
@@ -0,0 +1,31 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $n1 = $_POST['num1'];
+    $n2 = $_POST['num2'];
+    $op = $_POST['operation'];
+
+    switch ($op) {
+        case 'add':
+            $result = $n1 + $n2;
+            echo "Result: $result";
+            break;
+			case 'sub':
+            $result = $n1 - $n2;
+            echo "Result: $result";
+            break;
+			case 'div':
+            if ($n2 == 0) {
+                echo "Error: Division by zero!";
+            } else {
+                $result = $n1 / $n2;
+                echo "Result: $result";
+            }
+            break;
+			case 'mul':
+            $result = $n1 * $n2;
+            echo "Result: $result";
+            break;
+
+    }
+}
+?>
