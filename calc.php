3852538806b29aa0f0941df385b9f39bd233739f
Pradeep yadav <pradeep.yadav@enigmaticsmile.com>
2025-04-13 01:11:43 +0530

    Added addition functionality

---
+++ calc.php
@@ -0,0 +1,14 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $n1 = $_POST['num1'];
+    $n2 = $_POST['num2'];
+    $op = $_POST['operation'];
+
+    switch ($op) {
+        case 'add':
+            $result = $n1 + $n2;
+            echo "Result: $result";
+            break;
+    }
+}
+?>
