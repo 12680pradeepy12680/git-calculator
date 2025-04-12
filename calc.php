c685573a061787e283a4a3bbb8a3b5443a7a8e0b
Pradeep yadav <pradeep.yadav@enigmaticsmile.com>
2025-04-13 01:13:01 +0530

    Added subtraction functionality

---
+++ calc.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $n1 = $_POST['num1'];
+    $n2 = $_POST['num2'];
+    $op = $_POST['operation'];
+
+    switch ($op) {
+        case 'add':
+            $result = $n1 + $n2;
+            echo "Result: $result";
+            break;
+			case 'sub':
+            $result = $n1 - $n2;
+            echo "Result: $result";
+            break;
+    }
+}
+?>
